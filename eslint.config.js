942c991fe9f6552af738947240a9f67c5e5e4784
Jeff Nyman <jeffnyman@gmail.com>
2025-08-03 02:02:08 -0500

    Add storybook workshop.

--- eslint.config.js
+++ eslint.config.js
@@ -6,30 +6,34 @@ import reactDom from "eslint-plugin-react-dom";
 import reactX from "eslint-plugin-react-x";
 import prettierEslint from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
+import storybook from "eslint-plugin-storybook";
 import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-      reactHooks.configs["recommended-latest"],
-      reactX.configs["recommended-typescript"],
-      reactDom.configs.recommended,
-      reactRefresh.configs.vite,
-      prettierEslint,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
+export default tseslint.config(
+  [
+    globalIgnores(["dist"]),
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [
+        js.configs.recommended,
+        ...tseslint.configs.strictTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
+        reactHooks.configs["recommended-latest"],
+        reactX.configs["recommended-typescript"],
+        reactDom.configs.recommended,
+        reactRefresh.configs.vite,
+        prettierEslint,
+      ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        parserOptions: {
+          projectService: true,
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
     },
-  },
-]);
+  ],
+  storybook.configs["flat/recommended"],
+);
